2903d6a172a0a0eda9474817137b3f3512880990
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2019-05-12 22:53:57 +0530

    Creates Card Register Screen UI

---
+++ app/src/main/java/me/mqueiroz/picpay/ui/card/priming/CardRegisterPrimingFragment.kt
@@ -0,0 +1,24 @@
+package me.mqueiroz.picpay.ui.card.priming
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_card_register.*
+import me.mqueiroz.picpay.R
+
+class CardRegisterPrimingFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_card_register_priming, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        toolbar.setNavigationOnClickListener {
+            activity?.onBackPressed()
+        }
+    }
+}
