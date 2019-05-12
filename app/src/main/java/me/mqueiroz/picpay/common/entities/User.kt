05a7abd37dba197c260b30409e80bd8bb937cc03
Matheus de Mello Queiroz <m.dimello@hotmail.com>
2019-05-12 18:39:57 +0530

    Creates User entity

--- app/src/main/java/me/mqueiroz/picpay/common/entities/User.kt
+++ app/src/main/java/me/mqueiroz/picpay/common/entities/User.kt
@@ -1,5 +1,17 @@
 package me.mqueiroz.picpay.common.entities
 
+import com.squareup.moshi.Json
+
 data class User(
-    val name: String
+    @Json(name = "img")
+    val img: String? = null,
+
+    @Json(name = "name")
+    val name: String? = null,
+
+    @Json(name = "id")
+    val id: Int? = null,
+
+    @Json(name = "username")
+    val username: String? = null
 )
